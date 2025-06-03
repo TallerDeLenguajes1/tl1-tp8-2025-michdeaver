ed8731d7fc07b9509d88be6dfa09bb19f4216415
MichDeaver <michellecamilledeaver@gmail.com>
2025-06-03 13:06:45 -0300

    class tarea

---
+++ ToDo/list.cs
@@ -0,0 +1,7 @@
+public class Tarea{
+    public int TareaID {get;set}
+    public string Description {get;set}
+    public int Duracion {get;set} // validar que este entre 10 y 100
+    //se puede añadir un contructor y metodos auxiliares si lo considera necesario
+}
+
